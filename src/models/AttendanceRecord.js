dfef445fea2bd2ad56d8e01e3d70c3b0536fb0e3
cheivoy <cherrycywong0907@gmail.com>
2025-06-17 15:41:35 +0800

    更新 AttendanceRecord.js

--- src/models/AttendanceRecord.js
+++ src/models/AttendanceRecord.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const attendanceRecordSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     battleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Battle', required: true },
-    attended: { type: Boolean, required: true },
+    battleDate: { type: Date, required: true },
+    group: { type: String },
+    team: { type: String },
+    attended: { type: Boolean, default: false },
     timestamp: { type: Date, default: Date.now }
 });
 
