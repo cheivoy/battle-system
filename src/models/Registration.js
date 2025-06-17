1319ac7393528d272856a57d14c0013f5ceb66a4
cheivoy <cherrycywong0907@gmail.com>
2025-06-17 18:37:42 +0800

    更新 Registration.js

--- src/models/Registration.js
+++ src/models/Registration.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const registrationSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     battleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Battle', required: true },
-    timestamp: { type: Date, default: Date.now },
     isProxy: { type: Boolean, default: false },
     proxyReason: { type: String },
     proxyBy: { type: String },
-    isAlternate: { type: Boolean, default: false }
+    isWaitlist: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Registration', registrationSchema);
