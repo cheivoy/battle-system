d6ccf3f7bbfb2fcd2cff905ba9449d2d607b8f1a
cheivoy <cherrycywong0907@gmail.com>
2025-06-17 18:23:09 +0800

    更新 User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
-    discordUsername: { type: String, required: true },
-    gameId: { type: String, unique: true, sparse: true },
+    gameId: { type: String, unique: true },
     job: { type: String },
     isAdmin: { type: Boolean, default: false },
     onLeave: { type: Boolean, default: false }
